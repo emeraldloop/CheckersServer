09f229695f7ac489ef9c407f93a22a6b0d07ee81
emeraldloop <sergey.nikishin.2000@mail.ru>
2021-07-06 00:28:43 +0300

    Добавьте файлы проекта.

---
+++ CheckersServer/PlayerObject.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace CheckersServer
+{
+    class PlayerObject
+    {
+        protected internal string Id { get; private set; }
+        string userName;
+        TcpClient client;
+        ServerObject server;
+        public NetworkStream Stream;
+        string team;
+        public PlayerObject(TcpClient tcpClient, ServerObject serverObject)
+        {   
+            
+            client = tcpClient;
+            server = serverObject;
+            serverObject.AddConnection(this);
+            Id = ServerObject.connectedUsers.ToString();
+            
+        }
+
+        public void CheckIn()
+        {
+            try
+            {
+                Stream = client.GetStream();
+                string message = GetMessage();
+                if (Id == "1")
+                    team = " (Black team)";
+                userName = message + team;
+                if (Id == "2")
+                    team = " (White team)";
+                userName = message + team;
+                message = userName + " was connected";
+                server.BroadcastMessage(message, Id);
+                Console.WriteLine(message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private string GetMessage()
+        {
+            try
+            {
+                byte[] data = new byte[64]; //буфер для получаемых данных
+                StringBuilder builder = new StringBuilder();
+                int bytes = 0;
+
+                do              // получаем сообщение
+                {
+                    bytes = Stream.Read(data, 0, data.Length);
+                    builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                }
+                while (Stream.DataAvailable);
+
+                return builder.ToString();
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        // закрытие подключения
+        protected internal void Close()
+        {
+            if (Stream != null)
+                Stream.Close();
+            if (client != null)
+                client.Close();
+        }
+    }
+}
---
+++ CheckersServer/ServerObject.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+
+
+namespace CheckersServer
+{
+    class ServerObject
+    {
+        public NetworkStream Stream { get; set; }
+        static TcpListener tcpListener; //сервер для прослушивания
+        public static int connectedUsers = 0; //счетчик подключенных клиентов (в данный момент)
+
+        static List<PlayerObject> players = new List<PlayerObject>(); // все подключения
+        // прослушивание входящих подключений
+        protected internal void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, 8888);
+                tcpListener.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                    PlayerObject playerObject = new PlayerObject(tcpClient, this);
+                    playerObject.CheckIn();
+                    if (connectedUsers == 2)
+                        break;
+                }
+                ManagerObject managerObject = new ManagerObject();
+                Thread managerThread = new Thread(new ThreadStart(managerObject.Process));
+                managerThread.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+        protected internal void AddConnection(PlayerObject playerObject)
+        {
+            players.Add(playerObject);
+            ServerObject.connectedUsers++;
+        }
+
+        // отключение всех клиентов-
+        protected internal void Disconnect()
+        {
+            tcpListener.Stop(); // остановка сервера
+            for (int i = 0; i < players.Count; i++)
+            {
+                players[i].Close(); // отключение клиента 
+            }
+            Environment.Exit(0); // завершение процесса
+        }
+
+        protected internal void BroadcastMessage(string message, string id)
+        {
+            for (int i = 0; i < players.Count; i++)
+            {
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                players[i].Stream.Write(data, 0, data.Length); // передача данных
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
